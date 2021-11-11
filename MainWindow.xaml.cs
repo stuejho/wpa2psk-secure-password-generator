032746a346ad9636ea43171b9cf59b49c561b21a
Ethan Ho <ethan.j.ho-1@ou.edu>
2021-11-10 19:56:41 -0600

    Update load password GUI layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Windows;
@@ -49,31 +50,52 @@ namespace wpa2psk_secure_password_generator
             }
         }
 
-        private void loadPwdBtn_Click(object sender, RoutedEventArgs e)
+        private void loadPwdFileBtn_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
-                // Get IV and ciphertext from file
-                byte[] ivPlusCipher = File.ReadAllBytes(openFileDialog.FileName);
-
-                // Generate key
-                byte[] key = Vault.GeneratePrivateKey(passphraseLoadTextBox.Text);
-
-                // Decrypt password using the generated key
-                string password;
-                try
-                {
-                    password = Vault.DecryptStringFromBytes_AesCBC(ivPlusCipher, key);
-                }
-                catch (CryptographicException)
-                {
-                    password = "Error: Invalid key or corrupted password file";
-                }
-
-                // Display password to user
-                loadPwdTextBox.Text = password;
+                passwordFileTextBox.Text = openFileDialog.FileName;
             }
+        }
+
+        private void loadPwdBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Store decrypted password
+            string password;
+
+            // Get IV and ciphertext from file (return if file is not found)
+            byte[] ivPlusCipher;
+            try
+            {
+                ivPlusCipher = File.ReadAllBytes(passwordFileTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ArgumentException || ex is IOException)
+                {
+                    loadPwdTextBox.Text = "Error: Password file not found";
+                    return;
+                }
+                throw;
+            }
+
+            // Generate key
+            byte[] key = Vault.GeneratePrivateKey(passphraseLoadTextBox.Text);
+
+            // Decrypt password using the generated key
+            try
+            {
+                password = Vault.DecryptStringFromBytes_AesCBC(ivPlusCipher, key);
+            }
+            catch (CryptographicException)
+            {
+                loadPwdTextBox.Text = "Error: Invalid key or corrupted password file";
+                return;
+            }
+
+            // Display password to user
+            loadPwdTextBox.Text = password;
         }
     }
 }
